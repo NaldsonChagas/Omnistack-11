2c06a4d785c1986b61c8669ac21c0bc40f1f252a
Naldson Chagas <naldson.bc@gmail.com>
2020-03-29 01:03:10 -0300

    tela de login, component register e rotas

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import './style.css';
+
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+export default function Logon() {
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero!" />
+
+        <form action="">
+          <h1>Faça seu login</h1>
+
+          <input placeholder="Sua Id" />
+          <button type="submit" className="button">Entrar</button>
+
+          <Link to="/register">
+            <FiLogIn size={16} color="#e02041" /> Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
+}
---
+++ frontend/src/routes.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react';
+import Logon from "./pages/Login";
+import Register from "./pages/Register";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Logon} exact />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
